341a22b3f982f5c30853ed71d0408ca5a919b0ea
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-05-31 00:23:07 +0100

    Create button component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,17 @@
 import { Container, Slogan, Title } from './styles';
 
 import backgroundImg from '../../assets/background.png'
+import { Button } from '../../components/Button';
 
 export function SignIn() {
   return (
     <Container source={backgroundImg}>
       <Title>Fast Fleet</Title>
+
       <Slogan>Vehicle use management</Slogan>
+
+      <Button title='Entrar com Google' />
+
     </Container>
   );
 }
