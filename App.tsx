a354dcebe6d1d404242cac7ea1a1e0a0e8520747
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-05-30 23:50:16 +0100

    Configure styled-components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components/native'
+
+import theme from './src/theme';
+
 import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
   return (
-    <SignIn />
+    <ThemeProvider theme={theme} >
+      <SignIn />
+    </ThemeProvider>
   );
 }
