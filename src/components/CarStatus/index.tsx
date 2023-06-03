aa94a66994aa087b1f9a70733eb6543c50aeb494
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-06-03 02:52:25 +0100

    Navigate to departure screen

--- src/components/CarStatus/index.tsx
+++ src/components/CarStatus/index.tsx
@@ -1,12 +1,13 @@
 import { useTheme } from 'styled-components';
+import { TouchableOpacityProps } from 'react-native';
 import { Container, IconBox, Message, TextHighlight } from './styles';
 import { Key, Car } from 'phosphor-react-native'
 
-type Props = {
+type Props = TouchableOpacityProps & {
   licensePlate?: string;
 }
 
-export function CarStatus({ licensePlate }: Props) {
+export function CarStatus({ licensePlate, ...rest }: Props) {
   const theme = useTheme();
 
   const Icon = licensePlate ? Car : Key;
@@ -14,7 +15,7 @@ export function CarStatus({ licensePlate }: Props) {
   const status = licensePlate ? 'arrival' : 'departure';
 
   return (
-    <Container>
+    <Container {...rest}>
       <IconBox>
         <Icon size={32} color={theme.COLORS.BRAND_LIGHT} />
       </IconBox>
---
+++ src/screens/Departure/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from './styles';
+
+export function Departure() {
+  return (
+    <Container>
+    </Container>
+)};
