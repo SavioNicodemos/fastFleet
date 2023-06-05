dee8cd5bba60ca4670f05158dae72746e528a7b5
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-06-05 22:02:09 +0100

    Create license plate input

---
+++ src/components/LicensePlateInput/index.tsx
@@ -0,0 +1,25 @@
+import { useTheme } from 'styled-components';
+import { Container, Input, Label } from './styles';
+import { TextInputProps } from 'react-native';
+
+type Props = TextInputProps & {
+  label: string;
+}
+
+export function LicensePlateInput({ label, ...rest }: Props) {
+  const { COLORS } = useTheme();
+  return (
+    <Container>
+      <Label>
+        {label}
+      </Label>
+
+      <Input
+        maxLength={7}
+        autoCapitalize='characters'
+        placeholderTextColor={COLORS.GRAY_400}
+        {...rest}
+      />
+    </Container>
+  )
+};
--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,10 +1,18 @@
 import { Header } from '../../components/Header';
-import { Container } from './styles';
+import { LicensePlateInput } from '../../components/LicensePlateInput';
+import { Container, Content } from './styles';
 
 export function Departure() {
   return (
     <Container>
       <Header title='Departure' />
 
+      <Content>
+        <LicensePlateInput
+          label='License Plate'
+          placeholder='ABC-1234'
+        />
+      </Content>
     </Container>
-)};
+  )
+};
