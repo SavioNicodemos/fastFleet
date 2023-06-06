59d2e29b6359c0be3310448cd8a8f270b1f6bfc4
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-06-06 21:52:18 +0100

    Fetch item from local database

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,47 @@
-import { Container, Content } from './styles';
-import { HomeHeader } from '../../components/HomeHeader';
-import { CarStatus } from '../../components/CarStatus';
+import { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+import { HomeHeader } from '../../components/HomeHeader';
+import { CarStatus } from '../../components/CarStatus';
+import { useQuery } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
+
+import { Container, Content } from './styles';
+
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
   const navigation = useNavigation();
+
+  const historic = useQuery(Historic);
 
   const handleRegisterMovement = () => {
     navigation.navigate('departure');
   };
+
+  const fetchVehicle = () => {
+    try {
+      const vehicle = historic?.filtered("status = 'departure'")[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Vehicle in use', 'Unable to load the vehicle in use.');
+    }
+  };
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
+
   return (
     <Container>
       <HomeHeader />
 
       <Content>
-        <CarStatus onPress={handleRegisterMovement} />
+        <CarStatus
+        licensePlate={vehicleInUse?.license_plate}
+        onPress={handleRegisterMovement}
+        />
       </Content>
     </Container>
   )
