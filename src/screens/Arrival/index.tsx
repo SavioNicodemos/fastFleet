de4fa3c9ec54069956812e162ed30b04242aac94
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-06-06 23:12:49 +0100

    Delete register from local database

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,13 +1,14 @@
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { X } from 'phosphor-react-native';
 import { BSON } from 'realm';
+import { Alert } from 'react-native';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { ButtonIcon } from '../../components/ButtonIcon';
 
 import { Container, Content, Description, Footer, Label, LicensePlate } from './styles';
-import { useObject } from '../../libs/realm';
+import { useObject, useRealm } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
 
 type RouteParams = {
@@ -19,6 +20,31 @@ export const Arrival = () => {
   const { id } = route.params as RouteParams;
 
   const historic = useObject(Historic, new BSON.UUID(id));
+  const realm = useRealm();
+  const { goBack } = useNavigation();
+
+  const handleRemoveVehicleUsage = () => {
+    Alert.alert(
+      'Cancel',
+      'Are you sure you want to cancel this vehicle usage?',
+      [
+        {
+          text: 'No',
+          style: 'cancel',
+        },
+        {
+          text: 'Yes',
+          onPress: () => removeVehicleUsage(),
+        },
+      ])
+  }
+
+  const removeVehicleUsage = () => {
+    realm.write(() => {
+      realm.delete(historic);
+    });
+    goBack();
+  };
 
   return (
     <Container>
@@ -42,7 +68,7 @@ export const Arrival = () => {
         </Description>
 
         <Footer>
-          <ButtonIcon icon={X} />
+          <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
           <Button title="Register Arrival" />
         </Footer>
       </Content>
