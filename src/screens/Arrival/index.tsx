a639d86006a651120ee985fe5b7b3225b552eb57
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-06-06 22:26:20 +0100

    Finished styles in arrival screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,5 +1,11 @@
 import { useRoute } from '@react-navigation/native';
-import { Container, Title } from './styles';
+import { X } from 'phosphor-react-native';
+
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import { Container, Content, Description, Footer, Label, LicensePlate } from './styles';
 
 type RouteParams = {
   id: string;
@@ -11,9 +17,30 @@ export const Arrival = () => {
 
   return (
     <Container>
-      <Title>
-        id: {id}
-      </Title>
+      <Header title="Arrival" />
+
+      <Content>
+        <Label>
+          License plate
+        </Label>
+
+        <LicensePlate>
+          XXX-1234
+        </LicensePlate>
+
+        <Label>
+          Purpose
+        </Label>
+
+        <Description>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, quod eaque eius minus distinctio quae, eum, tempore labore aliquam facere illum aperiam laudantium esse. Iure necessitatibus magnam corporis unde voluptas.
+        </Description>
+
+        <Footer>
+          <ButtonIcon icon={X} />
+          <Button title="Register Arrival" />
+        </Footer>
+      </Content>
     </Container>
   )
 };
