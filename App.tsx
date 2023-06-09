10dcea375c8863a1222c3877cc2c4d6197985b65
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-06-09 01:07:25 +0100

    Install dayJs

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import 'react-native-get-random-values';
+import './src/libs/dayjs'
 
 import { ThemeProvider } from 'styled-components/native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
