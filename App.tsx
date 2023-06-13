fe4e0f4ad249430c545b9e7230664b15639b329d
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-06-13 13:57:43 +0100

    use NetInfo to show offline status

--- App.tsx
+++ App.tsx
@@ -1,24 +1,26 @@
 import 'react-native-get-random-values';
 import './src/libs/dayjs'
 
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { AppProvider, UserProvider } from '@realm/react'
+import { WifiSlash } from 'phosphor-react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useNetInfo } from '@react-native-community/netinfo';
 
 import { REALM_APP_ID } from '@env'
 import theme from './src/theme';
 
 import { SignIn } from "./src/screens/SignIn";
 import { Loading } from './src/components/Loading';
-import { StatusBar } from 'react-native';
 import { Routes } from './src/routes';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RealmProvider, syncConfig } from './src/libs/realm';
 import { TopMessage } from './src/components/TopMessage';
-import { WifiSlash } from 'phosphor-react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+  const netInfo = useNetInfo();
 
   if (!fontsLoaded) {
     return (
@@ -35,10 +37,14 @@ export default function App() {
             translucent
           />
 
-          <TopMessage
-            title="You are off-line."
-            icon={WifiSlash}
-          />
+          {
+            !netInfo.isConnected && (
+              <TopMessage
+                title="You are off-line."
+                icon={WifiSlash}
+              />
+            )
+          }
 
           <UserProvider fallback={SignIn}>
             <RealmProvider sync={syncConfig} fallback={Loading}>
