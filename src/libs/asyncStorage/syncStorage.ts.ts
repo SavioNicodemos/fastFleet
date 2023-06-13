c52ef13991b17a811d9d6e4d17e74925ec02cf0a
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-06-13 15:47:13 +0100

    Function to save last sync made locally

---
+++ src/libs/asyncStorage/syncStorage.ts.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const STORAGE_ASYNC_KEY = "@fastfleet:last_sync";
+
+export async function saveLastSyncTimestamp(){
+  const timestamp = new Date().getTime();
+
+  await AsyncStorage.setItem(STORAGE_ASYNC_KEY, timestamp.toString());
+
+  return timestamp;
+}
+
+export async function getLastSyncTimestamp(){
+  const timestamp = await AsyncStorage.getItem(STORAGE_ASYNC_KEY);
+
+  return timestamp ? Number(timestamp) : null;
+}
