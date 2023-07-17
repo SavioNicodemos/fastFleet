0acc6c726c349b31012f016fbe9c22f958660887
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-07-18 00:14:58 +0100

    Add google maps api key

---
+++ app.config.js
@@ -0,0 +1,51 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+module.exports = {
+  expo: {
+    name: "fastfleet",
+    slug: "fastfleet",
+    scheme: "fastfleet",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "dark",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "cover",
+      backgroundColor: "#202024",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#202024",
+      },
+      package: "com.savionicodemos.fastfleet",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: [
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission:
+            "Allow FastFleet to access your location.",
+        },
+      ],
+    ],
+  },
+};
--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -2,4 +2,5 @@ declare module '@env' {
   export const GOOGLE_ANDROID_CLIENT_ID: string;
   export const GOOGLE_IOS_CLIENT_ID: string;
   export const REALM_APP_ID: string;
+  export const GOOGLE_MAPS_API_KEY: string;
 }
