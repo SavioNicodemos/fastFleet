474ab86b16ff4b5dd7c2356a88c596601fdf31e4
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-08-19 18:48:21 +0100

    Create background task with expo-task-manager

---
+++ src/tasks/backgroundLocationtask.ts
@@ -0,0 +1,25 @@
+import { startLocationUpdatesAsync, Accuracy } from "expo-location";
+import * as TaskManager from "expo-task-manager";
+
+export const BACKGROUND_TASK_NAME = "location-tracking";
+
+TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
+  try {
+    if (error) {
+      throw error;
+    }
+
+    const { coords, timestamp } = data.locations[0];
+
+    const currentLocation = {
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+      timestamp,
+    };
+
+    console.log(currentLocation);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
