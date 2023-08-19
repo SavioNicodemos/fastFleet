dc34fb56c44470844fb89512af3349890026ed84
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-08-19 18:54:55 +0100

    Create methods to start/stop task

--- src/tasks/backgroundLocationtask.ts
+++ src/tasks/backgroundLocationtask.ts
@@ -1,4 +1,9 @@
-import { startLocationUpdatesAsync, Accuracy } from "expo-location";
+import {
+  startLocationUpdatesAsync,
+  hasStartedLocationUpdatesAsync,
+  Accuracy,
+  stopLocationUpdatesAsync,
+} from "expo-location";
 import * as TaskManager from "expo-task-manager";
 
 export const BACKGROUND_TASK_NAME = "location-tracking";
@@ -23,3 +28,37 @@ TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
   }
 });
 
+export async function startLocationTask() {
+  try {
+    const hasStarted = await hasStartedLocationUpdatesAsync(
+      BACKGROUND_TASK_NAME
+    );
+
+    if (hasStarted) {
+      await stopLocationTask();
+    }
+
+    await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
+      accuracy: Accuracy.Highest,
+      distanceInterval: 1,
+      timeInterval: 1000,
+      showsBackgroundLocationIndicator: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function stopLocationTask() {
+  try {
+    const hasStarted = await hasStartedLocationUpdatesAsync(
+      BACKGROUND_TASK_NAME
+    );
+
+    if (hasStarted) {
+      await stopLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
