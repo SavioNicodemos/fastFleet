97d637e87df69d85a053b3f92a7f3d4d8e5fed71
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-08-20 16:48:52 +0100

    Save coordinates locally

--- src/tasks/backgroundLocationtask.ts
+++ src/tasks/backgroundLocationtask.ts
@@ -5,13 +5,18 @@ import {
   stopLocationUpdatesAsync,
 } from "expo-location";
 import * as TaskManager from "expo-task-manager";
+import { saveStorageLocation } from "../libs/asyncStorage/locationStorage";
 
 export const BACKGROUND_TASK_NAME = "location-tracking";
 
-TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async ({ data, error }: any) => {
   try {
     if (error) {
       throw error;
+    }
+
+    if (!data) {
+      return;
     }
 
     const { coords, timestamp } = data.locations[0];
@@ -22,9 +27,10 @@ TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
       timestamp,
     };
 
-    console.log(currentLocation);
+    await saveStorageLocation(currentLocation);
   } catch (error) {
     console.error(error);
+    stopLocationTask();
   }
 });
 
